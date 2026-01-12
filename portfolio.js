c5c30062f3e9de161a6920e415202a9e4372563e
Jameel Mohammed <jameeel01@gmail.com>
2026-01-11 23:01:33 -0800

    Create scrolling animation in portfolio.js. Add description of portfolio. Update navbar to add drop shadow.

---
+++ portfolio.js
@@ -0,0 +1,13 @@
+// Add smooth scrolling to all links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
